5f32d298bd8b1bb4433a52e771db97d83b998907
Sergey Rusinov <70644336+Niko13teen@users.noreply.github.com>
2022-09-29 13:44:39 +0700

    Add files via upload

---
+++ recon_dir.py
@@ -0,0 +1,33 @@
+import argparse
+import requests
+
+
+def request(url):
+	try:
+		return requests.get("https://" + url)
+	except requests.exceptions.ConnectionError:
+		pass
+
+def recon_dir(file_name, target_url):
+	file = open(file_name, 'r')
+	for line in file:
+		directory = line.strip()
+		full_url = target_url + '/' + directory
+		response = request(full_url)
+		if response:
+			print('[+] Discovered Directory At This Path: ' + full_url)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--target', '-t',
+						action='store',
+						dest='target',
+						help='Enter target')
+	parser.add_argument('--file', '-f',
+						action='store',
+						dest='file',
+						help='file input')
+	args = parser.parse_args()
+	target_url, file_name = args.target, args.file
+	recon_dir(file_name, target_url)
